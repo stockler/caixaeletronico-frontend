13d59abf31bf15eb6808d3bcff3f3b7c9b708821
Rafael Stockler <rafael.stockler@gmail.com>
2017-01-19 23:37:24 -0200

    Added e2e tests

--- test/e2e/atm/atm.spec.js
+++ test/e2e/atm/atm.spec.js
@@ -1,7 +1,46 @@
-describe('Atm', function() {
-  it('should have one or more atms', function() {
-    browser.get('http://localhost:8080/');
 
-    expect(element.all(by.repeater('cash in vm.results')).count()).toBeGreaterThan(1);
+
+describe('Atm', function() {
+  it('should send value to server', function() {
+    browser.get('http://localhost:8080/withdrawal');
+
+    element(by.model('vm.withdrawalValue')).sendKeys('200');
+
+    // Find the first (and only) button on the page and click it
+    element(by.id("withdrawal")).click();
+
+    function eventual(expectedCondition) {
+	  return browser.wait(expectedCondition, 2000).then(function() {
+	    return true;
+	  }, function() {
+	    return false;
+	  });
+	}
+
+    var EC = protractor.ExpectedConditions;
+
+	var e = element(by.id('msg'));
+	expect(eventual(EC.presenceOf(e))).toBe(true);
+
+	expect(e.isPresent()).toBeTruthy();
+
+    
+  });
+
+  it('should have msg', function() {
+    
+  	function eventual(expectedCondition) {
+	  return browser.wait(expectedCondition, 2000).then(function() {
+	    return true;
+	  }, function() {
+	    return false;
+	  });
+	}
+	var EC = protractor.ExpectedConditions;
+	var e = element(by.id('msg'));
+	
+	expect(eventual(EC.presenceOf(e))).toBe(true);
+
+    expect(e.getText()).toEqual("Você sacou 200 reais!");
   });
 });
